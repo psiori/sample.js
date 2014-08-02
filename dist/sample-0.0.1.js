7e51563a074ecebc0d65a9f2628be9e3a5768f9b
salange <sascha@5dlab.com>
2014-08-02 12:00:33 +0200

    layout according to coding conventions

--- dist/sample-0.0.1.js
+++ dist/sample-0.0.1.js
@@ -149,7 +149,8 @@ var mergeParams = function(userParams, eventName)
 {
   var params = {};
   
-  var add = function(key, value) {
+  var add = function(key, value) 
+  {
     // we'll add "0" but ignore value that equals null
     if (key && typeof value !== "undefined" && (typeof value === "number" || value)) {
       params[key] = value;
@@ -170,7 +171,8 @@ var mergeParams = function(userParams, eventName)
   add("content_ids",    userParams.content_ids);
   
   if (eventName === "sessionStart" ||
-      eventName === "sessionUpdate") {
+      eventName === "sessionUpdate") 
+  {
     add("email",        userParams.email || email);
     add("platform",     userParams.platform || platform);
   }
@@ -178,11 +180,14 @@ var mergeParams = function(userParams, eventName)
   return params;
 };
 
-var randomToken = function(length) {
+var randomToken = function(length) 
+{
   var str = "";
   
-  for (var i=0; i < length; ++i) {
-    if (i > 0 && i % 4 === 0) {
+  for (var i=0; i < length; ++i) 
+  {
+    if (i > 0 && i % 4 === 0) 
+    {
       str += "-";
     }
     str += Math.floor(16*Math.random()).toString(16).toUpperCase();
@@ -191,7 +196,8 @@ var randomToken = function(length) {
   return str;
 };
 
-var Sample = {
+var Sample = 
+{
   safariOnly: false,
   
   PLATFORM_BROWSER:  'browser',
@@ -200,129 +206,173 @@ var Sample = {
   PLATFORM_WINDOWS:  'windwos',
   PLATFORM_FACEBOOK: 'facebook',
   
-  init: function(params) {
-    if (localStorage.SampleToken) {
+  init: function(params) 
+  {
+    if (localStorage.SampleToken) 
+    {
       installToken = localStorage.SampleToken;
     }
-    else {
+    else 
+    {
       localStorage.SampleToken = installToken = randomToken(24);
     }
-    if (sessionStorage.SampleToken) {
+    if (sessionStorage.SampleToken) 
+    {
       sessionToken = sessionStorage.SampleToken;
     }
-    else {
+    else 
+    {
       sessionStorage.SampleToken = sessionToken = randomToken(32);
     }
     platform = this.PLATFORM_BROWSER;
   },
   
-  setEndpoint: function(newEndpoint) {
+  stop: function() 
+  {
+    connector.stop();
+  },
+  
+  resume: function() 
+  {
+    connector.start();
+  },
+  
+  setEndpoint: function(newEndpoint) 
+  {
     endpoint = newEndpoint;
   },
 
-  getEndpoint: function() {
+  getEndpoint: function() 
+  {
     return endpoint;
   },
 
-  setAppToken: function(newAppToken) {
+  setAppToken: function(newAppToken) 
+  {
     appToken = newAppToken;
   },
   
-  setModule: function(newModule) {
+  setModule: function(newModule) 
+  {
     module = newModule;
   },
   
-  setPlatform: function(newPlatform) {
+  setPlatform: function(newPlatform) 
+  {
     platform = newPlatform;
   },
   
-  setUserId: function(newUserId) {
+  setUserId: function(newUserId) 
+  {
     userId = newUserId;
   },
   
-  setEmail: function(newEmail) {
+  setEmail: function(newEmail) 
+  {
     email = newEmail;
   },
   
-  setDebug: function(flag) {
+  setDebug: function(flag) 
+  {
     debug = flag;
   },
   
-  startGroup: function() {
+  startGroup: function() 
+  {
     connector.startGroup();
   },
   
-  endGroup: function() {
+  endGroup: function() 
+  {
     connector.endGroup();
   },
 
-  track: function(eventName, params) {
-    if (this.safariOnly === true && !this.isSafari()) {
+  track: function(eventName, params) 
+  {
+    if (this.safariOnly === true && !this.isSafari()) 
+    {
       return ;
     }
     params = mergeParams(params || {}, eventName);
     connector.add(params, function() { });
   },
   
-  sessionStart: function(newAppToken) {
+  sessionStart: function(newAppToken) 
+  {
     appToken = newAppToken || appToken;
     this.track('session_start', { event_category: 'session' });
   },
   
-  sessionUpdate: function() {
+  sessionUpdate: function() 
+  {
     this.track('session_update', { event_category: 'session' });
   },
   
-  ping: function() {
+  ping: function() 
+  {
     this.track('ping', { event_category: 'session' });
   },
   
   /** starts or stops autopinging every seconds seconds.
    * pass seconds = 0 to stop pinging. */
-  autoPing: function(seconds) {
+  autoPing: function(seconds) 
+  {
     var that = this;
-    if (typeof seconds === "undefined") {
+    if (typeof seconds === "undefined") 
+    {
       seconds = 60;
     }
     clearTimeout(autoping);
-    if (seconds && seconds > 0) {
-      autoping = setInterval(function() {
+    if (seconds && seconds > 0) 
+    {
+      autoping = setInterval(function() 
+      {
         that.ping();
       }, seconds * 1000);
     }
   },
   
-  contentUsage: function(content_ids, content_type) {
+  contentUsage: function(content_ids, content_type) 
+  {
     content_type = content_type || 'content';
     var args = { 
       content_type: content_type,
       event_category: 'content'
     };
-    if (Array.isArray(content_ids)) {
+    if (Array.isArray(content_ids)) 
+    {
       args.content_ids = content_ids;
     }
-    else {
+    else 
+    {
       args.content_id = content_ids;      
     }
     this.track('usage', args);
   },
   
-  isSafari: (function() {
+  isSafari: (function() 
+  {
     var memo = null;
     
-    return function() {
-      if (memo === null) {
+    return function() 
+    {
+      if (memo === null) 
+      {
         var ua = navigator.userAgent.toLowerCase(); 
         
-        if (ua.indexOf('safari') != -1) { 
-          if (ua.indexOf('chrome') > -1) {
+        if (ua.indexOf('safari') != -1) 
+        { 
+          if (ua.indexOf('chrome') > -1) 
+          {
             memo = false;
           } 
-          else {
+          else 
+          {
             memo = true;
           }
         }
-        else {
+        else 
+        {
           memo = false;
         }
       }
